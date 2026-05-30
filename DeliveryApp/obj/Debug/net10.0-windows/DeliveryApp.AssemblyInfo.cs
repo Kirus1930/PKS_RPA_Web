0772ec98cebc8a35f5fc829e8037de30c2e46829
Kirus1930 <k_eryomin00@mail.ru>
2026-05-30 15:31:30 +0300

    dotnet build

--- DeliveryApp/obj/Debug/net10.0-windows/DeliveryApp.AssemblyInfo.cs
+++ DeliveryApp/obj/Debug/net10.0-windows/DeliveryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eac8279e8eb3db9ed97a6e1670fe36d5efa10e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a588d1c9843440db1798ca8c7a3212755b16e77")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
